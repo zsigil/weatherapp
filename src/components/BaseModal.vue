07c82b5a7166e42996d82bbfbc37eba80247d086
Zsigil <chickpox@freemail.hu>
2022-10-24 17:55:10 +0200

    transition and teleport

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,21 +1,27 @@
 <template>
-  <div
-    v-show="modalActive"
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-  >
-    <div
-      v-if="modalActive"
-      class="self-start p-4 bg-white mt-32 max-w-screen-md"
-    >
-      <slot />
-      <button
-        @click="$emit('close-modal')"
-        class="mt-8 py-2 px-6 text-white bg-weather-primary"
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
       >
-        Close
-      </button>
-    </div>
-  </div>
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="self-start p-4 bg-white mt-32 max-w-screen-md"
+          >
+            <slot />
+            <button
+              @click="$emit('close-modal')"
+              class="mt-8 py-2 px-6 text-white bg-weather-primary"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script setup>
@@ -28,4 +34,26 @@ defineProps({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
